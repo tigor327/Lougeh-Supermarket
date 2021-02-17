74b148c6b907f4c3a9b03d509df4cd7b422b5b71
tigor327 <43983528+tigor327@users.noreply.github.com>
2021-02-17 14:37:52 +0800

    Update lougeh.sql

--- lougeh.sql
+++ lougeh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2021 at 08:24 PM
+-- Generation Time: Feb 17, 2021 at 07:35 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `lougeh`
+CREATE DATABASE lougeh;
 --
 
 -- --------------------------------------------------------
